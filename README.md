b026133a9a85fc8d87353724790ce1b9d48319c9
akh <akh@mkakh.com>
2017-10-15 04:58:01 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dict
+
+This is a simple dictionary script.
+
+## Usage
+```
+$ ./dict.sh [search|add|delete] word
+```
+Only when adding a new word, you also can do as follows.
+```
+$ ./dict.sh add word the_definition
+```
---
+++ dict.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -eu
+DICT_FILE=dict.csv
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 [search|add|delete] word"
+    exit 1
+fi
+
+if [ $1 = "add" ] || [ $1 = "a" ]; then
+    if [ $# -eq 3 ]; then
+        DEFINITION=$3
+    else
+        echo "What is the definition?"
+        printf "> " 
+        read DEFINITION 
+    fi
+    printf "%s,%s\n" "$2" "$DEFINITION" >> $DICT_FILE
+elif [ $1 = "search" ] || [ $1 = "s" ]; then
+    grep -w "$2" $DICT_FILE
+elif [ $1 = "delete" ] || [ $1 = "d" ]; then
+    sed "/^$2,.*$/d" -i $DICT_FILE
+else
+    echo "Usage: $0 [search|add|delete] word"
+    exit 1
+fi
